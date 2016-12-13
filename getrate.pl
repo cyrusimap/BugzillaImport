ceb8fef7cc64955b1719bf84ea22596b7856093c
Bron Gondwana <brong@fastmail.fm>
2016-12-13 15:01:25 +1100

    getrate: script to show rate limits

---
+++ getrate.pl
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use Net::GitHub;
+use Data::Dumper;
+use XML::Fast;
+use HTML::Entities;
+use DateTime::Format::DateParse;
+use Encode qw(decode_utf8 encode_utf8);
+use JSON;
+
+BEGIN {
+    Net::GitHub::V3::Issues->__build_methods(
+        import_issue => { url => "/repos/%s/%s/import/issues", method => 'POST', args => 1 },
+    );
+};
+
+open(FH, "<oauth_token.txt");
+my $token = <FH>;
+chomp($token);
+close(FH);
+
+my $gh = Net::GitHub::V3->new(
+  access_token => $token,
+);
+
+$gh->ua->default_header(Accept => 'application/vnd.github.golden-comet-preview+json');
+
+my $issue = $gh->issue;
+
+my $res = $issue->query('GET', '/rate_limit');
+print Dumper($res);
+
